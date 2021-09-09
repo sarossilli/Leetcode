2e4297fd664bc7ff2970e97a89e7423b11e64008
Sam Rossilli <60826965+sarossilli@users.noreply.github.com>
2021-09-08 22:03:10 -0600

    Time: 900 ms (56.42%), Space: 76.5 MB (8.15%) - LeetHub

--- lru-cache/lru-cache.py
+++ lru-cache/lru-cache.py
@@ -1,31 +1,71 @@
-from collections import OrderedDict
-    
-class LRUCache:
-
-    def __init__(self, capacity: int):
-        self.capacity = capacity
-        self.data = OrderedDict()
+class DLinkedNode(): 
+    def __init__(self):
+        self.key = 0
+        self.value = 0
+        self.prev = None
+        self.next = None
         
+class LRUCache:
+    def _add_node(self,node):
+        node.prev = self.head
+        node.next = self.head.next
+        
+        self.head.next.prev = node
+        self.head.next = node
+        
+    def _remove_node(self,node):
+        prev = node.prev
+        new = node.next
+        prev.next = new
+        new.prev = prev
+        
+    def _addFront(self,node):
+        self._remove_node(node)
+        self._add_node(node)
+        
+    def _popEnd(self):
+        res = self.tail.prev
+        self._remove_node(res)
+        return res
+    
+    def __init__(self, capacity: int):
+        self.head = DLinkedNode()
+        self.tail = DLinkedNode()
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        self.lru = dict()
+        self.size = 0
+        self.cap = capacity
 
-
+        
     def get(self, key: int) -> int:
-        if key in self.data:
-            self.data.move_to_end(key)
-            return self.data[key]
+        node = self.lru.get(key,None)
+        if node:
+            self._addFront(node)
+            return node.value
         else:
             return -1
-        print(self.data)
-
+        
+        
+    
     def put(self, key: int, value: int) -> None:
-        if key in self.data:
-            self.data[key] = value
-        elif len(self.data) >= self.capacity:
-            self.data.popitem(0)
-            self.data[key] = value
+        node = self.lru.get(key,None)
+        if node:
+            node.value = value
+            self._addFront(node)
         else:
-            self.data[key] = value
-        self.data.move_to_end(key)
+            newNode = DLinkedNode()
+            newNode.key = key
+            newNode.value = value
+            self.lru[key] = newNode
+            self._add_node(newNode)
+            self.size +=1
             
+            if self.size > self.cap:
+                tail = self._popEnd()
+                del self.lru[tail.key]
+                self.size -= 1
+
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
