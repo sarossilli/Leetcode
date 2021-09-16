3a732b776b1613dbba2140b59a9804453ba382b9
Sam Rossilli <60826965+sarossilli@users.noreply.github.com>
2021-09-15 22:21:15 -0600

    Time: 76 ms (16.46%), Space: 14.3 MB (28.15%) - LeetHub

--- integer-to-roman/integer-to-roman.py
+++ integer-to-roman/integer-to-roman.py
@@ -1,32 +1,25 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        dic = {
-            0:'',
-            1:'I',
-            4:'IV',
-            5:'V',
-            9:'IX',
-            10:'X',
-            40:'XL',
-            50:"L",
-            90:"XC",
-            100:"C",
-            400:"CD",
-            500:"D",
-            900:"CM",
-            1000:"M"
-        }
-        vals = [k for k in dic.keys()]
-        vals = vals[::-1]
-        print(vals)
-        i = 0
-        ans = ''
-        while num != 0 and i!=13:
-
-            toAdd = int(num/vals[i])
-            num = num % vals[i]
-            for k in range(toAdd):
-                ans += dic[vals[i]]
-            i+=1
         
-        return ans
+        digits = [(1000, "M"), (900, "CM"), (500, "D"), (400, "CD"), (100, "C"), 
+                  (90, "XC"), (50, "L"), (40, "XL"), (10, "X"), (9, "IX"), 
+                  (5, "V"), (4, "IV"), (1, "I")]
+
+        res = []
+        for i,letter in digits:
+            if num == 0:
+                break
+            
+            count = num//i
+            num = num%i
+            
+            res.append(letter * count)
+            
+            
+        
+        return "".join(res)
+    
+    
+    #101
+    # c:1, r=1
+    # I
