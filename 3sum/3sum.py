4798a45d165a0fc37c2682f9f4e6e183a4bc37b1
Sam Rossilli <60826965+sarossilli@users.noreply.github.com>
2021-09-15 20:06:53 -0600

    Time: 1229 ms (46.39%), Space: 17.4 MB (73.38%) - LeetHub

--- 3sum/3sum.py
+++ 3sum/3sum.py
@@ -1,24 +1,28 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        ans = []
+        res = []
+        
         nums.sort()
         
-        for i,val in enumerate(nums):
-            if i > 0 and val == nums[i-1]:
-                continue
+        for i in range(len(nums)):
             
-            l, r = i+1, len(nums)-1
-            
-            while(l<r):
-                three = val + nums[l] + nums[r]
-                if three >0:
-                    r -=1
-                elif three < 0:
-                    l +=1
-                else:
-                    ans.append([val,nums[l],nums[r]])
-                    l+=1
-                    while nums[l] == nums[l-1] and l<r:
-                        l+=1
-                        
-        return ans
+            if i==0 or nums[i] != nums[i-1]:
+                self.twoSum(nums,i,res)
+        
+        return res
+    
+    def twoSum(self, nums: List[int], i: int, res: List[List[int]]):
+        l, r = i + 1, len(nums) - 1
+        
+        while (l < r):
+            sum = nums[i] + nums[l] + nums[r]
+            if sum < 0:
+                l += 1
+            elif sum > 0:
+                r -= 1
+            else:
+                res.append([nums[i], nums[l], nums[r]])
+                l += 1
+                r -= 1
+                while l < r and nums[l] == nums[l - 1]:
+                    l += 1
