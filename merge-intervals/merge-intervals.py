dd8780013cc4b7c9679c22773c342a2f711cf747
Sam Rossilli <60826965+sarossilli@users.noreply.github.com>
2021-09-22 11:38:31 -0600

    Time: 84 ms (81.36%), Space: 16.1 MB (53.97%) - LeetHub

--- merge-intervals/merge-intervals.py
+++ merge-intervals/merge-intervals.py
@@ -1,40 +1,17 @@
 class Solution:
-
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        # sort the keys
-        intervals.sort(key=lambda x: x[0])
-        arr = intervals
-        pointer = 0
-        while len(arr) > pointer+1:
-            pointer = check(arr, pointer)
-        return arr
+        #sort
+        intervals.sort(key = lambda x: x[0])
+        merged = []
+        for interval in intervals:
+            if merged and merged[-1][1] >= interval[0]:
+                merged[-1][1] = max(merged[-1][1],interval[1])
+            else:
+                merged.append(interval)
+        return merged       
     
-def check(arr,pointer):
-    a,b = arr[pointer]
-    c,d = arr[pointer+1]
-    
-    # a <= c (sorted)
-    
-    #overlaping [1,2] [2,4]
-    # b >= c ?
-    
-    #included [1,4] [2,3]
-    # b >= c ?
-    # b >= d
-
-	# check if element N+1 is within element N
-	# Example: [1, 5], [2, 4]
-    if c in range(a, b+1) and d in range(a, b+1):
-        arr.pop(pointer+1)
-        return pointer
-    
-    # check if the start of element N+1 is in range of element N and end is not in range of it.
-	# example: [1, 5], [2, 6]
-    elif c in range(a, b+1) and not d in range(a, b+1):
-        arr.pop(pointer+1)
-        arr[pointer] = [a, d]
-        return pointer
-    
-    return pointer+1
-    
-    return pointer+1
+#[[1,3],[2,6],[8,10],[15,18]]
+# L = 1, h = 3
+# 1<2<3 SO MERGE? -> [1,6]
+# L = 1 H = 6
+# 1<6<8 SO NO MERGE
