034999b13fe9661a970808fe66a2d62099d5b81d
Sam Rossilli <60826965+sarossilli@users.noreply.github.com>
2021-10-02 19:55:17 -0600

    Time: 1796 ms (74.12%), Space: 14.2 MB (86.12%) - LeetHub

---
+++ confusing-number-ii/confusing-number-ii.py
@@ -0,0 +1,23 @@
+class Solution:
+    def confusingNumberII(self, n: int) -> int:
+        flip = {
+            0:0, 
+            1:1, 
+            6:9,
+            8:8,
+            9:6
+        }
+        
+        self.res = 0
+
+        def dfs(num, numRotated, unit):
+            if num != numRotated:
+                self.res += 1
+
+            for d, dRotated in flip.items():
+                if d == 0 and num == 0: continue  # Skip zero infinity!
+                if num * 10 + d > n: break  # Over N already!
+                dfs(num * 10 + d, dRotated * unit + numRotated, unit * 10)
+
+        dfs(0, 0, 1)
+        return self.res
