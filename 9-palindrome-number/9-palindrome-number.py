83d65fa9a797918cf55d9dd927db5b40ff1271c3
Sam Rossilli <60826965+sarossilli@users.noreply.github.com>
2022-01-25 13:34:27 -0700

    Time: 119 ms (13.62%), Space: 14.3 MB (15.95%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x<0:
+            return False
+        
+        s = str(x)
+        l = 0
+        r = len(s)-1
+
+        
+        while(l<r):
+            if s[l] != s[r]:
+                return False
+            else:
+                l+=1
+                r-=1
+                
+        return True
