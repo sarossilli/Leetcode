b6b75a2fdb7a774a4cc836ff242423ca3bae5f79
Sam Rossilli <60826965+sarossilli@users.noreply.github.com>
2022-02-07 18:13:12 -0700

    Time: 165 ms (32.40%), Space: 20 MB (10.03%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,13 +1,9 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        ans = dict()
-        for w in strs:
-            count = [0]*26
-            for c in w:
-                count[ord(c)-ord('a')] += 1
-            hashVal = tuple(count)
-            if hashVal in ans:
-                ans[hashVal].append(w)
-            else:
-                ans[hashVal] = [w]
+        ans = collections.defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            ans[tuple(count)].append(s)
         return ans.values()
